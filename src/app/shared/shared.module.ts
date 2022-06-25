252faec83b3c947742f81669adfad0332c8c9419
Hamza Hayd <dado.org@hotmail.com>
2022-06-25 15:16:25 +0200

    Added first batch of form validators

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,24 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabcontainerComponent } from './tabcontainer/tabcontainer.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+//import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
-  declarations: [ModalComponent, TabcontainerComponent, TabComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent, TabcontainerComponent, TabComponent],
+  declarations: [
+    ModalComponent,
+    TabcontainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [
+    ModalComponent,
+    TabcontainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
   //providers: [ModalService], //Array of services. Injecting manually
 })
 export class SharedModule {}
