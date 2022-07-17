a1f9ba2d2526a1d1b9eaa9668eca0e84321b6157
Hamza Farah <hamza.farah@aditro.com>
2022-07-17 18:07:39 +0200

    added some more custom validators to the register-form

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
-import { FormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import IUser from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
-
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-  showAlert = false;
-  alertMsg = 'Please wait! Your account is being created';
-  alertColor = 'blue';
+  constructor(private auth: AuthService, private emailTaken: EmailTaken) {}
 
   inSubmission = false;
 
-  constructor(private auth: AuthService) {}
-
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('', [Validators.required, Validators.email]);
+  email = new FormControl(
+    '',
+    [Validators.required, Validators.email],
+    [this.emailTaken.validate]
+  );
   age = new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
@@ -28,25 +29,31 @@ export class RegisterComponent {
     Validators.required,
     Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
   ]);
-  confirm_passowrd = new FormControl('', [Validators.required]);
+  confirm_password = new FormControl('', [Validators.required]);
   phoneNumber = new FormControl('', [
     Validators.required,
-    Validators.minLength(10),
+    Validators.minLength(13),
     Validators.maxLength(13),
   ]);
+  showAlert = false;
+  alertMsg = 'Please wait! Your account is being created.';
+  alertColor = 'blue';
 
-  registerForm = new UntypedFormGroup({
-    name: this.name,
-    email: this.email,
-    age: this.age,
-    password: this.password,
-    confirm_passowrd: this.confirm_passowrd,
-    phoneNumber: this.phoneNumber,
-  });
+  registerForm = new FormGroup(
+    {
+      name: this.name,
+      email: this.email,
+      age: this.age,
+      password: this.password,
+      confirm_password: this.confirm_password,
+      phoneNumber: this.phoneNumber,
+    },
+    [RegisterValidators.match('password', 'confirm_password')]
+  );
 
   async register() {
     this.showAlert = true;
-    this.alertMsg = 'Please wait! Your account is being created';
+    this.alertMsg = 'Please wait! Your account is being created.';
     this.alertColor = 'blue';
     this.inSubmission = true;
 
@@ -55,7 +62,7 @@ export class RegisterComponent {
     } catch (e) {
       console.error(e);
 
-      this.alertMsg = 'An unexpected error occured. Please try again later';
+      this.alertMsg = 'An unexpected error occurred. Please try again later';
       this.alertColor = 'red';
       this.inSubmission = false;
       return;
